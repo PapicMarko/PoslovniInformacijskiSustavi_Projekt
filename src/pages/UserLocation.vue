29b39499b15f465972f5eeffd9abf0e27eefa3bb
Loris Lukić <loris.lukic13@gmail.com>
2022-06-19 15:29:06 +0200

    Rađenje boxa za upisivanje adrese
    Co-Authored-By: Socrates <32815437+TheKingpin221@users.noreply.github.com>

---
+++ src/pages/UserLocation.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="ui two column centered grid">
+        <div class="column red">
+            <form class="ui segment large form">
+                <div class="ui message red"></div>
+                <div class="ui segment"></div>
+                    <div class="field">
+                       <div class="ui right icon input large">
+                            <input type="text" placeholder="Enter your address" />
+                            <i class="dot circle link icon"></i>
+                       </div>
+                    </div>
+                    <button class="ui button">GO</button>
+            </form>
+        </div>
+    </section>
+</template>
