5f59d56c634990bab42e3dfcd5b6ebe82949d241
Loris Lukić <loris.lukic13@gmail.com>
2022-06-23 21:17:33 +0200

    Mali napredak
    Co-Authored-By: Socrates <32815437+TheKingpin221@users.noreply.github.com>

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,87 +1,68 @@
 <template>
-<div>
-    <section class="ui two column centered grid" style="position:relative; z-index:1;">
-        <div class="column red">
-            <form class="ui segment large form">
-                <div class="ui message red" v-show="error">{{error}}</div>
-                <div class="ui segment">
-                    <div class="field">
-                       <div class="ui right icon input large" :class="{loading:spinner}">
-                            <input 
-                                type="text" 
-                                placeholder="Enter your address" 
-                                v-model="address"
-                                id="autocomplete" 
-                            />
-                            <i class="dot circle link icon" @click="locatorButtonPressed"></i>
-                       </div>
+    <div>
+        <section class="ui two column centered grid" style="position:relative; z-index:1;">
+            <div class="column green">
+                <form class="ui segment large form">
+                    <div class="ui message red" v-show="error">{{error}}</div>
+                    <div class="ui segment">
+                        <div class="field">
+                            <div class="ui right icon input large" :class="{loading:spinner}">
+                                <input 
+                                    type="text" 
+                                    placeholder="Enter your address" 
+                                    v-model="address"
+                                    id="autocomplete" 
+                                />
+                                <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+                        </div>
+                        </div>
                     </div>
-                    <button class="ui button">GO</button>
-                </div>
-            </form>
-        </div>
-    </section>
-    <section id="map"></section>
-</div>
+                    <button class="ui button" @click="showUserLocationOnTheMap">POKAŽI</button>
+                </form>
+            </div>            
+        </section>
+        <section id="map"></section>
+    </div>
 </template>
 
 
 <script>
 import axios from 'axios'
-
-
-
+var pok3;
+var pok4;
 export default {
-
     data(){
         return {
             address: "",
             error: "",
-            spinner: false
+            spinner: false,          
         };
     },
 
     
-
-/*     mounted() {
-        new google.maps.places.Autocomplete(
-            document.getElementById("autocomplete"),
-                {
-                bounds: new google.maps.LatLngBounds(
-                    new google.maps.LatLng(44.86833, 13.84806)
-                )
-            }    
-        )
-    },  */
-
-
+    
     methods : {
 
-            getAddressFrom(lat, long) {
-                axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyCc_CY_DWbBD8xWnZALfvX2Fhi8vTpvZes")
-                .then(response => {
-                    if(response.data.error_message){
-                        this.error=response.data.error_message;
-                        console.log(response.data.error_message);
+        getAddressFrom(lat, long) {
+            axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyCc_CY_DWbBD8xWnZALfvX2Fhi8vTpvZes")
+            .then(response => {
+                if(response.data.error_message){
+                    this.error=response.data.error_message;
+                    console.log(response.data.error_message);
+                } else {
+                    this.address=response.data.results[0].formatted_address
+                    console.log(response.data.results[0].formatted_address);
+                }
+                this.spinner=false;
+            })
+            .catch(error => {
+                this.error= error.message;
+                this.spinner=false;
+                console.log(error.message);
+            })
 
-                    } else {
-                        this.address=response.data.results[0].formatted_address
-                        console.log(response.data.results[0].formatted_address);
-                   
-
-                    }
-                    this.spinner=false;
-                })
-                .catch(error => {
-                    this.error= error.message;
-                    this.spinner=false;
-                    console.log(error.message);
-                })
-
-            },
-
-
-
+        },
+                        
 
         locatorButtonPressed() {
 
@@ -89,19 +70,29 @@ export default {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
                     position => {
-                        console.log(position.coords.latitude),
-                        console.log(position.coords.longitude),
+                        //console.log(position.coords.latitude),
+                        //console.log(position.coords.longitude),
+                        position.coords.latitude,
+                        position.coords.longitude,
+                        //pok3 = position.coords.latitude,
+                        //pok4 = position.coords.longitude,
+                        console.log(this.pok1)
+                        //console.log(this.pok2)
+                        //console.log(this.pok3)
+                        //console.log(this.pok4)
                         this.getAddressFrom(
                             position.coords.latitude,
                             position.coords.longitude
                         );
                         this.showUserLocationOnTheMap(position.coords.latitude, position.coords.longitude)
                     },
+
+                    
                     error => {
                         this.error="Molim Vas upišite adresu.";
                         this.spinner=false;
                         console.log(error.message);
-                    }
+                    },
                 );
             } else {
                 this.error=error.message;
@@ -145,30 +136,23 @@ export default {
             )
         }, */
 
-       showUserLocationOnTheMap(latitude,longitude) {
-            let map = new google.maps.Map(document.getElementById("map"), {
+        showUserLocationOnTheMap(latitude, longitude, google) {
+/*             pok3=pok1
+            pok4=this.pok2; */
+            let map = new google.maps.Map(document.getElementById("map"),  {
                 zoom: 15,
                 center: new google.maps.LatLng(latitude,longitude),
-                mapTypeId:google.maps.MapTypeId.ROADMAP
-            })
-        }
+                mapTypeId: google.maps.MapTypeId.ROADMAP    
+            });
+        } 
     }
-} 
+};
 </script>
 <style>
 .ui.button,
 .dot.circle.icon {
-    background-color: #ff5a5f;
+    background-color: #c3d618;
     color: white;
-}
-
-#map {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(177, 224, 238, 0.767);
 }
 
 .pac-icon{
@@ -190,6 +174,13 @@ export default {
 }
 
 
-
+#map {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(255, 255, 255, 0.767);
+}
 
 </style>
